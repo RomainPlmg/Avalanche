3a0fc05bea98e570114f02e3df329040dcf6a42d
Romain-Plmg <plumaugat.romain78@gmail.com>
2024-08-19 19:19:04 +0200

    Remove a useless return after a fatal error

--- Avalanche/src/Avalanche/Renderer/GraphicsContext.cpp
+++ Avalanche/src/Avalanche/Renderer/GraphicsContext.cpp
@@ -11,7 +11,6 @@ std::unique_ptr<GraphicsContext> GraphicsContext::Create(void* window) {
     switch (Renderer::GetAPI()) {
         case RendererAPI::API::None:
             AVL_CORE_FATAL("Please select a graphic API");
-            return nullptr;
         case RendererAPI::API::OpenGL:
             return std::make_unique<OpenGLContext>(static_cast<GLFWwindow*>(window));
         default:
